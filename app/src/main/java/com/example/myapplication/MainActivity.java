e7a424b21296263ed51833adf7c7bf1326a0e55e
sancoLgates <NK00596184@TechMahindra.com>
2019-06-17 17:41:29 +0700

    add snackbar, and adjust pan to prevent soft keyboard up the webview

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,14 +2,17 @@ package com.example.myapplication;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.webkit.WebView;
 import android.widget.Button;
 //import android.widget.EditText;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -49,8 +52,23 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.editText);
         String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGES, message);
-//        intent.putExtra(EXTRA_MESSAGE, "test");
-        startActivity(intent);
+
+        //pake spasi doank gk bisa submit, ajib
+        if(message.trim().equals("")){
+//        masih bisa submit isi pake spasi doank
+//        if(TextUtils.isEmpty(message)){
+            editText.setHint("pesan kosong");
+            editText.setError(getResources().getString(R.string.message_error));
+//            Toast.makeText(this, "message is empty Bosssssssssssssssssssssssssssssssssssssssssssss", Toast.LENGTH_SHORT).show(); //2 second delay
+            Toast.makeText(this, "message is empty Bosssssssssssssssssssssssssssssssssssssssssssss", Toast.LENGTH_LONG).show(); //3.5 second delay
+
+            int s = 6000;
+            Snackbar.make(view, "snackbar", Snackbar.LENGTH_LONG).setDuration(s).show();
+        } else {
+            intent.putExtra(EXTRA_MESSAGES, message);
+//          intent.putExtra(EXTRA_MESSAGE, "test");
+            startActivity(intent);
+        }
+
     }
 }
