ebe9c485511b67457e240b140f170971b3617ded
sancoLgates <NK00596184@TechMahindra.com>
2019-06-18 17:25:23 +0700

    login test with attempt

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.myapplication;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.Uri;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -16,8 +17,11 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button butt;
-    TextView hello;
+    Button butt, login, close;
+    TextView hello, total;
+    EditText username, password;
+
+    int counter = 3;
 
     public static final String EXTRA_MESSAGES = "com.example.myapplication.MESSAGE";
 
@@ -46,6 +50,41 @@ public class MainActivity extends AppCompatActivity {
                 webView.loadUrl("http://android.okhelp.cz/category/software/");
             }
         });
+
+//        LOGIN
+        login = (Button)findViewById(R.id.login);
+        close = (Button)findViewById(R.id.close);
+        username = (EditText)findViewById(R.id.username);
+        password = (EditText)findViewById(R.id.password);
+        total = (TextView)findViewById(R.id.attempt);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
+                    Toast.makeText(getApplicationContext(), "Redirecting ...", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Wrong Credentials", Toast.LENGTH_SHORT).show();
+
+                    total.setVisibility(View.VISIBLE);
+//                    total.setBackgroundColor(Color.RED);
+                    counter--;
+                    total.setText(String.format("%s%s", getResources().getString(R.string.attemptleft), Integer.toString(counter)));
+
+                    if(counter == 0) {
+                        login.setEnabled(false);
+                    }
+                }
+            }
+        });
+
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
     }
 
     public void sendMessage(View view) {
@@ -57,13 +96,13 @@ public class MainActivity extends AppCompatActivity {
         if(message.trim().equals("")){
 //        masih bisa submit isi pake spasi doank
 //        if(TextUtils.isEmpty(message)){
-            editText.setHint("pesan kosong");
+            editText.setHint(getResources().getString(R.string.message_kosong));
             editText.setError(getResources().getString(R.string.message_error));
 //            Toast.makeText(this, "message is empty Bosssssssssssssssssssssssssssssssssssssssssssss", Toast.LENGTH_SHORT).show(); //2 second delay
-            Toast.makeText(this, "message is empty Bosssssssssssssssssssssssssssssssssssssssssssss", Toast.LENGTH_LONG).show(); //3.5 second delay
+            Toast.makeText(this, getResources().getString(R.string.message_error), Toast.LENGTH_LONG).show(); //3.5 second delay
 
             int s = 6000;
-            Snackbar.make(view, "snackbar", Snackbar.LENGTH_LONG).setDuration(s).show();
+            Snackbar.make(view, getResources().getString(R.string.message_kosong), Snackbar.LENGTH_LONG).setDuration(s).show();
         } else {
             intent.putExtra(EXTRA_MESSAGES, message);
 //          intent.putExtra(EXTRA_MESSAGE, "test");
